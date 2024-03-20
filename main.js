e07a3ad522fa5be36bf1e8f23c10135b26cca4d8
saddamsheikh01 <saddamsheikh1177@gmail.com>
2024-03-20 20:59:57 +0500

    frist commit

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["Developer","Designer","Freelancer","Youtuber","Deep Learner"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
